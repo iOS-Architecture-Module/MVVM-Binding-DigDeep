1f9538dbb063f43248e824f350c9d977cb1e65f0
HaviLee <Havi.X.Li.-ND@disney.com>
2018-11-26 17:07:00 +0800

    initial repo

---
+++ MVVM-Binding-DigDeepTests/MVVM_Binding_DigDeepTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_Binding_DigDeepTests.swift
+//  MVVM-Binding-DigDeepTests
+//
+//  Created by Li, Havi X. -ND on 2018/11/26.
+//  Copyright © 2018 Li, Havi X. -ND. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_Binding_DigDeep
+
+class MVVM_Binding_DigDeepTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
